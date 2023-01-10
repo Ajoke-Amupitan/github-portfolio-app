3269ad0b1bd18cc85cf6fdfca8c6439ae301228c
*Ajoke <*amupitanajoke2019@gmail.com*>
2023-01-11 00:16:47 +0100

    App component updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
@@ -7,10 +6,8 @@ import Home from './components/Home';
 function App() {
   return (
     <div>
-      <BrowserRouter>
       <NavBar />
       <Home />
-      </BrowserRouter>
      
     </div>
   );
